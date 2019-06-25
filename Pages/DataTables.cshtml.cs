49adb023b9403de0db3a6c1cfc6a548c85176d66
Tam Ireland <tamireland@hotmail.com>
2019-06-25 02:03:38 +0100

    Add project files.

---
+++ Pages/DataTables.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SB_Admin_Razor.Pages
+{
+    public class DataTablesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
